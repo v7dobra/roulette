325106bb525c84572734dc411b558ede10aa7b3b
v7dobra <shherbina.i@gmail.com>
2017-05-12 18:06:52 +0300

    rullet

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<script src="https://code.jquery.com/jquery-3.0.0.js"></script>
+	<script src="script.js"></script>
+	<link  type="text/css" href="css/style.css">
+
+</head>
+<body>
+
+
+
+
+<div class="container"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
